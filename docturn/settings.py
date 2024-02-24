86687ab0680d77a60f22c72a3b335be1de4efa99
Tekkieware <tekkieware@gmail.com>
2024-02-24 13:37:04 +0100

    rest

--- docturn/settings.py
+++ docturn/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
     'authentication.apps.AuthenticationConfig',
     'api.apps.ApiConfig',
+
+
+    'rest_framework',
     
 ]
 
@@ -124,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

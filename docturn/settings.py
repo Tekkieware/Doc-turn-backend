908e46e3fc6bf753b96a59a51db29834ecdda893
Tekkieware <tekkieware@gmail.com>
2024-02-24 21:13:43 +0100

    djoser setup

--- docturn/settings.py
+++ docturn/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 
     'rest_framework',
+    'djoser',
     
 ]
 
@@ -130,3 +131,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+       
+    ),
+}
+
+DJOSER = {
+   'USER_CREATE_PASSWORD_RETYPE': True,
+
+
+   }
